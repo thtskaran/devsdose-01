18338b24da7ddee4dfc559f8dc261d55ae572bbe
thtskaran <hello@karanprasad.com>
2024-05-03 04:00:14 +0000

    Fix missing import in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 import { Button } from "./ui/button";
 import { buttonVariants } from "./ui/button";
 import { HeroCards } from "./HeroCards";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
 
 export const Hero = () => {
   return (
